d6e9a1a2814c2d4894c34f6aca42b59763601e51
Konstantinos <kgiantsios@gmail.com>
2018-01-14 19:47:08 +0000

    directory views and some forms

--- config/routes.js
+++ config/routes.js
@@ -9,6 +9,14 @@ const results = require('../controllers/result');
 
 
 router.get('/', (req, res) => res.render('statics/index'));
+router.get('/racers', (req, res) => res.render('statics/racers.ejs'));
+router.get('/users', (req, res) => res.render('statics/users.ejs'));
+router.get('/events', (req, res) => res.render('statics/events.ejs'));
+router.get('/results', (req, res) => res.render('statics/results.ejs'));
+router.get('/teams', (req, res) => res.render('statics/teams.ejs'));
+router.get('/addracer', (req, res) => res.render('racer/new.ejs'));
+router.get('/addevent', (req, res) => res.render('event/new.ejs'));
+router.get('/adduser', (req, res) => res.render('user/new.ejs'));
 
 
 
